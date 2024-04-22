5c144ad353cf3de3c37cd3622f388d8223a8c859
student <gonzalezjoshua925@gmail.com>
2024-04-21 22:43:13 -0700

    final

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # csc317-assignment-6
+
+# This project has given me a better understanding of javascript and it's comparison operators.
--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-
+document.getElementById('submitBtn').addEventListener('click', function() {
+    var textInput = document.getElementById('textInput').value;
+    var wordFrequency = {};
+  
+    // Tokenize the input text
+    var words = textInput.split(/\s+/);
+  
+    // Count word frequency
+    words.forEach(function(word) {
+      wordFrequency[word] = (wordFrequency[word] || 0) + 1;
+    });
+  
+    // Sort by frequency and then by word value
+    var sortedWords = Object.keys(wordFrequency).sort(function(a, b) {
+      // If frequencies are equal, choose the word with smaller string length
+      if (wordFrequency[a] === wordFrequency[b]) {
+        return a.length - b.length; // Compare string lengths
+      }
+      return wordFrequency[b] - wordFrequency[a]; // Otherwise, compare by frequency
+    });
+  
+    // Display top 5 most frequent words
+    var output = '<table>';
+    output += '<tr><th>Top Five</th><th>Most Frequent Words</th></tr>';
+    for (var i = 0; i < Math.min(sortedWords.length, 5); i++) {
+      var word = sortedWords[i];
+      output += '<tr><td>' + word + '</td><td>' + wordFrequency[word] + '</td></tr>';
+    }
+    output += '</table>';
+    document.getElementById('output').innerHTML = output;
+  
+    // Log frequency table object
+    console.log(wordFrequency);
+  });
